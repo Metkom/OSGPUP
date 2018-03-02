0a5d55c94d6d2b16a979f2ac8c968285f050102c
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-02 15:56:43 +0700

    Update magnet_dyke_model.m

--- Forward modeling/magnet_dyke_model.m
+++ Forward modeling/magnet_dyke_model.m
@@ -1,5 +1,6 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
+% Cite: I. Nugroho, et. al. 2018. Forward modelling metode geomagnet. http://doi.org/10.5281/zenodo.1186988
 
 clear; clc; close;
 
