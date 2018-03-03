800eba85cbad1eab233e400490981ac3f774152c
Mohammad Heriyanto <mheriyanto@alumni.itb.ac.id>
2018-03-04 01:30:15 +0700

    Update gravity_fault_model.m

--- Forward modeling/gravity_fault_model.m
+++ Forward modeling/gravity_fault_model.m
@@ -1,7 +1,6 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66293-forward-modeling-metode-gravity--fault-model
-% Cite: E.B.G. Umboh, et. al. 2018. Forward modelling metode gravitasi. http://doi.org/10.5281/zenodo.1186957 
 
 clear; close; clc;
 
