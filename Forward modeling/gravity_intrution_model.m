d55dd908afea277385a7148b049ec4b7ec455adf
Mohammad Heriyanto <mheriyanto@alumni.itb.ac.id>
2018-03-04 01:41:04 +0700

    Update gravity_intrution_model.m

--- Forward modeling/gravity_intrution_model.m
+++ Forward modeling/gravity_intrution_model.m
@@ -2,6 +2,7 @@
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66294-forward-modeling-metode-gravity--intrution-model
 % Cite: E.B.G. Umboh, et. al. 2018. Forward modelling metode gravitasi. http://doi.org/10.5281/zenodo.1186957 
+% Cek
 
 clear; clc;
 
