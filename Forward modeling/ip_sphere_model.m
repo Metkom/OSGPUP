038d4140c68ae87c2404dd25c971e15cbae01af6
Mohammad Heriyanto <mheriyanto@alumni.itb.ac.id>
2018-03-04 01:51:06 +0700

    Update ip_sphere_model.m

--- Forward modeling/ip_sphere_model.m
+++ Forward modeling/ip_sphere_model.m
@@ -2,6 +2,7 @@
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66295-forward-modeling-metode-induced-polarization--sphere-model
 % Cite: M.F.R. Auly, et. al. 2018. Forward modelling metode IP. http://doi.org/10.5281/zenodo.1187228
+% Cek Cek
 
 clear; close; clc;
 R = 18; 
