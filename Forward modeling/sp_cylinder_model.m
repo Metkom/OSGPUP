63ba8e251b2159b6b17952b2fe4f6d2e98b46e16
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 02:44:55 +0700

    Update sp_cylinder_model.m

--- Forward modeling/sp_cylinder_model.m
+++ Forward modeling/sp_cylinder_model.m
@@ -1,4 +1,4 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
-% Download: 
+% Download: https://www.mathworks.com/matlabcentral/fileexchange/66299-forward-modeling-metode-self-potensial--cylinder-model
 % Cite: D. Wijaya, et. al. 2018. Forward modeling metode SP. http://doi.org/10.5281/zenodo.1187020
