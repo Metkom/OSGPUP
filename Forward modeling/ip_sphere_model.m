1512ba81e6241516924290e604f969e75a03da37
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 01:26:37 +0700

    Update ip_sphere_model.m

--- Forward modeling/ip_sphere_model.m
+++ Forward modeling/ip_sphere_model.m
@@ -1,5 +1,6 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
+% Download: https://www.mathworks.com/matlabcentral/fileexchange/66295-forward-modeling-metode-induced-polarization--sphere-model
 % Cite: M.F.R. Auly, et. al. 2018. Forward modelling metode IP. http://doi.org/10.5281/zenodo.1187228
 
 clear; close; clc;
