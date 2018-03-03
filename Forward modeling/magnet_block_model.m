a309f2dbaeca4ef6a9d32f0e77a0a596fcd3ecb0
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 02:24:16 +0700

    Update magnet_block_model.m

--- Forward modeling/magnet_block_model.m
+++ Forward modeling/magnet_block_model.m
@@ -1,4 +1,4 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
-% Download: 
+% Download: https://www.mathworks.com/matlabcentral/fileexchange/66296-forward-modeling-metode-magnetik--block-model
 % Cite: I. Nugroho, et. al. 2018. Forward modelling metode geomagnet. http://doi.org/10.5281/zenodo.1186988
