3ceee488dd5e193de2363831a32402cc14ce68fe
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 02:02:03 +0700

    Update gravity_intrution_model.m

--- Forward modeling/gravity_intrution_model.m
+++ Forward modeling/gravity_intrution_model.m
@@ -3,67 +3,64 @@
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66294-forward-modeling-metode-gravity--intrution-model
 % Cite: E.B.G. Umboh, et. al. 2018. Forward modelling metode gravitasi. http://doi.org/10.5281/zenodo.1186957 
 
-clear; clc;
+clear; close; clc;
+%--- Parameter Model (rho, R)
+rho1 = 1;
+rho2 = 1;% Perbedaan densitas (kg/m^3)
 
-%--- Parameter Model (rho)
-rho   = 1;     % Perbedaan densitas (kg/m^3)
-
-%--- Konstanta
-z1    = 50;    % Kedalaman plat dalam (m)
-z2    = 150;   % Kedalaman plat dangkal
-x0    = 100;   % Letak patahan (m)
-t     = 40;    % Tebal patahan (m)
+%--- Konstanta 
+x0 = 50;
+m = 30;
+z = 100;
 cGrav = 6.674e-11;  % Konstanta Gravitasi (m^3 kg^-1 s^-2)
 si2mg = 1e5;        % 1 SI (ms^-2) = 1e5 mGal
-
 %--- Lokasi Pengukuran
-x = -500:20:500;
+x = 0:2:100;
 
-%%--- FORMULA FORWARD SPHERE
-k = 2*cGrav*rho*t;
+%%--- FORMULA FORWARD SQUARE 
+k1 = cGrav*rho1;
 for i = 1:length(x)
-  dg(i) = k*(pi+(atan((x(i)-x0)/z1))-(atan((x(i)-x0)/z2))); %ms^2
+  a = (m/2)+(x(i)-x0);      % Jarak Ke P(x,0)
+  b = (x(i)-x0)-(m/2);
+  dg1(i) = k1*(a*log((a^2+z^2)/a^2)-(b*log((b^2+z^2)/b^2))+((2*z)*(atan(a/z)- atan(b/z))));
 end
 
-%--- Forward modeling
-dg  = dg*si2mg;  %1 SI = 1e5 mGal
+k2 = cGrav*rho2;
+for i = 1:length(x)
+  a2 = (m/2)+(x(i)-x0);      % Jarak Ke P(x,0)
+  b2 = (x(i)-x0)-(m/2);
+  dg2(i) = k2*(a2*log((a2^2+z^2)/a2^2)-(b2*log((b2^2+z^2)/b2^2))+((2*z)*(atan(a2/z)- atan(b2/z))));
+end
 
-%%--------- PLOT FORWARD ---------%%
+%--- Forward modeling 1
+dg1  = dg1*si2mg;  %1 SI = 1e5 mGal
+dg2  = dg2*si2mg;  %1 SI = 1e5 mGal
+x = [-z,(-z-20),(-z-20),-z];
+y = [((x0/2)-(m/6)),((x0/2)-(m/6)),((x0/2)+(m/3)),((x0/2)+(m/3))];
+x1 = [-z,(-z-20),(-z-20),-z];
+y1 = [((x0/2)-(m/6)),((x0/2)-(m/6)),((x0/2)+(m/3)),((x0/2)+(m/3))];
 figure(1)
-subplot(2,1,1)
-plot(x,dg,'.','color','r','MarkerSize',15);
-xlim([min(x) max(x)]);
-xlabel('\bf \fontsize{12}\fontname{Times}Position (m)');
-ylabel('\bf \fontsize{12}\fontname{Times}\Delta g (mGal)');
-title ('\bf \fontsize{14}\fontname{Times}Modeling Gravity - Fault');
-
-% Gambar patahan
-subplot(2,1,2)
-% Zona 1
-left  = x(1);
-right = x0;
-top   = -z2;
-bottom= top-t;
-xx    = [left left right right];
-yy    = [bottom top top bottom];
-fill(xx,yy,'r');
+subplot(3,1,1)
+plot(dg1,'color','b')
 hold on
 
-% Zona 2
-left_2  = x0;
-right_2 = x(length(x));
-top_2   = -z1;
-bottom_2= top_2-t;
-xx      = [left_2 left_2 right_2 right_2];
-yy      = [bottom_2 top_2 top_2 bottom_2];
-fill(xx,yy,'r');
-xlim([min(x) max(x)]);
+subplot(3,1,1)
+plot(dg2,'color','r')
+xlabel('Posisi (m)')
+ylabel('Delta g (mGal)')
+title('Modeling Gravity - Intrusi')
+hold off
 
-title(['\bf \fontsize{12} \fontname{Times}Model => ','\rho = ', ...
-      num2str(rho),' kg/m^3 ; z_1 = ',num2str(z1),...
-      ' m ; z_2 = ',num2str(z2),' m ; t = ',num2str(t),' m ; x_0 = ',...
-      num2str(x0),' m']);
-xlabel('\bf \fontsize{12}\fontname{Times}Position (m) ' );
-ylabel('\bf \fontsize{12}\fontname{Times}Depth (m) ');
-xlim([min(x) max(x)]);
-axis equal;
+subplot(3,1,2)
+fill(y,x,'b')
+xlim([0 60]);
+ylim([-120 -50]);
+xlabel('Posisi (m)')
+ylabel('Kedalaman (m)')
+
+subplot(3,1,3)
+fill(y1,x1,'r')
+xlim([0 60]);
+ylim([-120 -50]);
+xlabel('Posisi (m)')
+ylabel('Kedalaman (m)')
