4a3bde27fd2fc63b110f8debe73c5ab47ee6af38
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 14:11:58 +0700

    Update sp_sphere_model.m

--- Forward modeling/sp_sphere_model.m
+++ Forward modeling/sp_sphere_model.m
@@ -1,5 +1,5 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
-# Download: https://www.mathworks.com/matlabcentral/fileexchange/66300-forward-modeling-metode-self-potensial--sphere-model
+% Download: https://www.mathworks.com/matlabcentral/fileexchange/66300-forward-modeling-metode-self-potensial--sphere-model
 % Cite: Cite: Wijaya, Darmawangsa; Kuncoro, Trio Adi; Hariyanto, Ucok Jhonatan; Pratama, Francisco Surya; 
 % Pangaribuan, Aulia Rahman (2018): Forward Modeling Metode Self Potensial. figshare. https://doi.org/10.6084/m9.figshare.5946724.v1
