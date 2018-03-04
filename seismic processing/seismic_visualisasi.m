8efcbad4685cc1c35c497a1fea8a43711943d5cc
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 10:55:15 +0700

    Update seismic_visualisasi.m

--- seismic processing/seismic_visualisasi.m
+++ seismic processing/seismic_visualisasi.m
@@ -1,4 +1,5 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66301-visualisasi-data-seismik-2d
-% Cite: I.P. Virginia, et. al. 2018. Visualisasi data seismik 2D. https://doi.org/10.6084/m9.figshare.5946649.v1
+% Cite: Virginia, Intang Pinki; Arahman, Nanditha; Laurens, Kinta Rahma Aulika; Pertiwi, Tiaraningtias Bagus; 
+%       Suryani, Adelia (2018): Visualisasi Data Seismik 2 Dimensi. figshare. https://doi.org/10.6084/m9.figshare.5946649.v1
