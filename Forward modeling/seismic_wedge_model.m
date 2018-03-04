da1ef50f7ed34b4546f530354620231c43b49a85
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 12:48:12 +0700

    Update seismic_wedge_model.m

--- Forward modeling/seismic_wedge_model.m
+++ Forward modeling/seismic_wedge_model.m
@@ -1,4 +1,6 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66298-wedge-model
-% Cite: N. Anugerah, et. al. 2018. Membuat model sintetik untuk model pembajian. http://doi.org/10.5281/zenodo.118649
+% Cite: Anugerah, Nisfu; Ginting, Gamaliel Rhema; Wicaksono, Gigih Aji; Salsabila, Alda; 
+% Subakti, Puguh Ari; Syahputra, Loris Alif (2018): Membuat Model Sintetik untuk Model Pembajian. 
+% figshare. https://doi.org/10.6084/m9.figshare.5946691.v1
