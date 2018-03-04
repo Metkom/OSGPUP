fd4117e662d4c7689caa12cc379757e465bad773
Komputasi Geofisika <32222885+Metkom@users.noreply.github.com>
2018-03-04 11:29:52 +0700

    Update seismic_data_spektrum.m

--- seismic processing/seismic_data_spektrum.m
+++ seismic processing/seismic_data_spektrum.m
@@ -1,4 +1,5 @@
 % Copyright: 2018 - Teknik Geofisika, Universitas Pertamina
 % URL: https://sites.google.com/site/metkomup/programming
 % Download: https://www.mathworks.com/matlabcentral/fileexchange/66302-spektrum-data-seismik-sebagai-fungsi-kedalaman
-% Cite: S.R. Aisy, et. al. 2018. Spektrum data seismik sebagai fungsi kedalaman. http://doi.org/10.5281/zenodo.1186465
+% Cite: Aisy, Safira Rihhadatul; Elsalama, Nadiah Faridah Firdausi; Anugrah, Sari Atika; Nabila, Afviya; 
+% Nugraha, Rama Desta (2018): Spektrum Data Seismik sebagai Fungsi Kedalaman. figshare. https://doi.org/10.6084/m9.figshare.5946658.v1
